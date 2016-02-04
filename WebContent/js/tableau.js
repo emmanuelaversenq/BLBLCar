f0782cf728875ca0a2f78892e4bd81b34e6d883d
eaversenq <emmanuel.aversenq@berger-levrault.fr>
2016-02-04 16:56:07 +0100

    Premier projet HelloWorld

--- WebContent/js/tableau.js
+++ WebContent/js/tableau.js
@@ -5,14 +5,9 @@
 window.onload = calculeRayon;
 
 function calculeRayon() {
-	init();
-	
 	var item = document.getElementsByTagName("tr");
-	for (var i=1; i < item.length; i++) {
-		var adrDep=item[i].children[4].innerHTML;
-		var adrArr=item[i].children[5].innerHTML;
-		var distance=codeAddress(adrDep, adrArr,item[i].children[6]);
-		//item[i].style.visibility= (distance > 2 ? "Hidden" : "visible");
-		
+
+	for (var i=0; i < item.childNodes.length; i++) {
+		item.childNodes(i)
 	}
 }
